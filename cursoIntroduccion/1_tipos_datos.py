f88e84d1c560e3ec636baeb792ce9cb21cc59249
NeoTRAN001 <neo_tran@outlook.com>
2020-01-10 20:22:02 -0800

    Se han agregado los scripts 1_tipos_datos, 2_variables.py, 3_variables

---
+++ cursoIntroduccion/1_tipos_datos.py
@@ -0,0 +1,7 @@
+print('\n************* Tipos de datos en Python *************\n')
+
+print('El tipo de dato para el numero 6 es: ', type(6))
+print('El tipo de dato para el numero 6.2 es: ', type(6.2))
+print('El tipo de dato para el texto Hola es: ', type('Hola'))
+print('El tipo de dato para el caracter C es: ', type('C'))
+print('El tipo de dato para el valor binario es: ', type(True))
---
+++ cursoIntroduccion/3_variables.py
@@ -0,0 +1,21 @@
+numero = 1
+
+print(numero + 4)
+
+numero = 9 * 8
+
+
+print(numero)
+
+numero = 'HOla'
+
+print(numero)
+
+numero2 = numero
+
+print(numero2)
+
+
+tipo = type(numero2)
+
+print(tipo)
