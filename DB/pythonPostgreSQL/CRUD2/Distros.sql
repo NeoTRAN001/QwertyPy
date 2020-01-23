0da95c632e8531d3c79653cc8cac6da9458d9cf8
NeoTRAN001 <neo_tran@outlook.com>
2020-01-23 11:41:06 -0600

    Diseno de la base de datos para el segundo CRUD

---
+++ DB/pythonPostgreSQL/CRUD2/Distros.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE Distro (
+	id INTEGER PRIMARY KEY,
+	distro_name VARCHAR(30),
+	kernel VARCHAR(30),
+	de_id INT
+);
+
+CREATE TABLE DE (
+	id INTEGER PRIMARY KEY,
+	de_name VARCHAR(10)
+);
+
+SELECT * FROM DE;
+SELECT * FROM Distro;
+
+INSERT INTO DE (id, de_name) 
+	VALUES (1, 'KDE'), (2, 'XFCE');
+
+INSERT INTO Distro (id, distro_name, kernel, de_id)
+	VALUES (1, 'MX Linux', '4.19', 2);
+
+SELECT D.id, D.distro_name, D.kernel, DE.de_name FROM Distro AS D
+	INNER JOIN DE ON D.de_id = DE.id;
+
+
